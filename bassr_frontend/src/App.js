e25c93b8d09dd56a286f2d68b3abb0942cde2073
xrojo <x.rojo@hotmail.com>
2021-03-10 10:02:04 -0600

    created main container

--- bassr_frontend/src/App.js
+++ bassr_frontend/src/App.js
@@ -1,81 +1,12 @@
-import { useState, useEffect } from "react";
-import Player from "./components/Player";
-import Songlist from "./components/songs/Songlist";
+import React from "react";
+import Login from "./components/Login";
+import MainContainer from "./components/MainContainer";
 
 function App() {
-  const [songs, setSongs] = useState([
-    {
-      name: "Sunday",
-      artist: { name: "Bloc Party" },
-      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/bloc.jpg",
-      song_url:
-        "https://arcosmedia.cc/wp-content/uploads/2020/12/sunday-bloc-party.mp3",
-    },
-    {
-      name: "Diamonds",
-      artist: { name: "Boxer Rebellion" },
-      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/boxer.jpg",
-      song_url:
-        "https://arcosmedia.cc/wp-content/uploads/2020/12/diamonds-boxer-rebellion.mp3",
-    },
-    {
-      name: "Unconditional",
-      artist: { name: "The Bravery" },
-      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/brave.jpg",
-      song_url:
-        "https://arcosmedia.cc/wp-content/uploads/2020/12/unconditional-the-bravery.mp3",
-    },
-  ]);
-
-  //   const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  //   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex +1);
-
-  //   useEffect(() => {
-  //     setNextSongIndex(() => {
-  //       if (currentSongIndex + 1 > songs.length -1) {
-  //         return 0;
-  //       } else {
-  //         return currentSongIndex + 1;
-  //       }
-  //     })
-  //   }, [currentSongIndex]);
-
-  //   return (
-  //     <div className="App">
-  //       <Player
-  //         currentSongIndex={currentSongIndex}
-  //         setCurrentSongIndex={setCurrentSongIndex}
-  //         nextSongIndex={nextSongIndex}
-  //         songs={songs}
-  //        />
-  //     </div>
-  //   );
-  // }
-
-  // export default App;
-
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [nextSongIndex, setNextSongIndex] = useState(0);
-
-  useEffect(() => {
-    setNextSongIndex(() => {
-      if (currentSongIndex + 1 > songs.length - 1) {
-        return 0;
-      } else {
-        return currentSongIndex + 1;
-      }
-    });
-  }, [currentSongIndex, songs.length]);
-
   return (
     <div className="App">
-      <Player
-        currentSongIndex={currentSongIndex}
-        setCurrentSongIndex={setCurrentSongIndex}
-        nextSongIndex={nextSongIndex}
-        songs={songs}
-      />
-      <Songlist setSongs={setSongs} />
+      <MainContainer />
+      <Login />
     </div>
   );
 }
---
+++ bassr_frontend/src/components/MainContainer.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import Player from "./Player";
+import Songlist from "./songs/Songlist";
+
+function MainContainer() {
+  const [songs, setSongs] = useState([
+    {
+      name: "Sunday",
+      artist: { name: "Bloc Party" },
+      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/bloc.jpg",
+      song_url:
+        "https://arcosmedia.cc/wp-content/uploads/2020/12/sunday-bloc-party.mp3",
+    },
+    {
+      name: "Diamonds",
+      artist: { name: "Boxer Rebellion" },
+      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/boxer.jpg",
+      song_url:
+        "https://arcosmedia.cc/wp-content/uploads/2020/12/diamonds-boxer-rebellion.mp3",
+    },
+    {
+      name: "Unconditional",
+      artist: { name: "The Bravery" },
+      img_src: "https://arcosmedia.cc/wp-content/uploads/2020/12/brave.jpg",
+      song_url:
+        "https://arcosmedia.cc/wp-content/uploads/2020/12/unconditional-the-bravery.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+      <Songlist setSongs={setSongs} />
+    </div>
+  );
+}
+
+export default MainContainer;
