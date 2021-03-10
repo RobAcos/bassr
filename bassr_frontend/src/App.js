3f1ac7b07758dcb4ea160c1e51d514818a5be3bd
xrojo <x.rojo@hotmail.com>
2021-03-10 10:19:54 -0600

    login working

--- bassr_frontend/src/App.js
+++ bassr_frontend/src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import Login from "./components/Login";
 import MainContainer from "./components/MainContainer";
 
 function App() {
   return (
-    <div className="App">
-      <MainContainer />
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/player">
+            <MainContainer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
